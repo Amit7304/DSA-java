4b0340050df432c6412142f5cff5a817e2789f10
Amit7304 <akrathore7304@gmail.com>
2026-04-06 03:33:57 +0530

    Fixed sorting logic and updated code

--- Sorting.java
+++ Sorting.java
@@ -1,43 +1,44 @@
-public class Sorting{
+import java.util.Arrays;
+
+public class Sorting {
 
     public static void selectionsort(int arr[]) {
-        for(int i=0;i<arr.length-1;i++){
-            int minPos=i;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[minPos]>arr[j]){
-                    minPos=j;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[minPos] > arr[j]) {
+                    minPos = j;
                 }
             }
             int temp = arr[minPos];
-            arr[minPos]=arr[i];
-            arr[i]=temp;
+            arr[minPos] = arr[i];
+            arr[i] = temp;
         }
     }
+
     public static void printArr(int arr[]) {
-        for(int i=0;i<arr.length;i++){
-            System.out.print(arr[i]+" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-
-
-
-
-    public static void insertionsort(int arr[]){
-        for(int i=1; i<arr.length;i++){
+    public static void insertionsort(int arr[]) {
+        for (int i = 1; i < arr.length; i++) {
             int curr = arr[i];
-            int prev=i-1;
-            while(prev>=0 && arr[prev]>curr){
-                arr[prev+1]=arr[prev];
+            int prev = i - 1;
+            while (prev >= 0 && arr[prev] > curr) {
+                arr[prev + 1] = arr[prev];
                 prev--;
             }
-            arr[prev+1]=curr;
+            arr[prev + 1] = curr;
         }
     }
-public static void main(String args[]){
-int arr[] = {5,2,4,3,1,6};
-insertionsort(arr);
-printArr(arr);
-}
+
+    public static void main(String args[]) {
+        int arr[] = { 5, 2, 4, 3, 1, 6 };
+        // insertionsort(arr);
+        Arrays.sort(arr);
+        printArr(arr);
+    }
 }
