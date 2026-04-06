51fa65e21d09c805ba5304e9d98507772d8e54ac
Amit7304 <akrathore7304@gmail.com>
2026-04-07 02:00:14 +0530

    Starting New topic Strings

---
+++ Strings.java
@@ -0,0 +1,9 @@
+public class Strings {
+    
+    public static void main(String[] args) {
+        String str="abcd";
+        String str2= new String("xyz");
+
+        // In java Strings are IMMUTABLE
+    }
+}
