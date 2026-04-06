c29f068074aa31108de28f62474cb8d7fcca675e
Amit7304 <akrathore7304@gmail.com>
2026-04-07 01:40:23 +0530

    Fixed sorting logic and updated code

--- Sorting.java
+++ Sorting.java
@@ -1,4 +1,3 @@
-import java.util.Arrays;
 
 public class Sorting {
 
@@ -35,10 +34,30 @@ public class Sorting {
         }
     }
 
+    public static void countingsort(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for(int i=0;i<arr.length;i++){
+            largest =Math.max(largest, arr[i]);
+        }
+
+        int count[] = new int[largest+1];
+        for(int i=0;i<arr.length;i++){
+            count[arr[i]]++;
+        }
+        int j=0;
+        for(int i=0;i<count.length;i++){
+            while(count[i]>0){
+                arr[j]=i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void main(String args[]) {
-        int arr[] = { 5, 2, 4, 3, 1, 6 };
-        // insertionsort(arr);
-        Arrays.sort(arr);
+        int arr[] = { 5, 2, 4, 3,1,3,4,2,6,7,8,6 };
+        countingsort(arr);
+        //Arrays.sort(arr);
         printArr(arr);
     }
 }
