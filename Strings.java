a2436d8e1fae9a622626cf7f8408a781e3e00dee
Amit7304 <akrathore7304@gmail.com>
2026-04-07 02:18:42 +0530

    Learn new things in Strings

--- Strings.java
+++ Strings.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Strings {
     
     public static void main(String[] args) {
@@ -5,5 +6,31 @@ public class Strings {
         String str2= new String("xyz");
 
         // In java Strings are IMMUTABLE
+
+        //Input/output in string
+        Scanner sc = new Scanner (System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println("This is a String=>"+name);
+
+
+        //string Length
+        String Fullname= "Amit Kumar";
+        System.out.println(Fullname.length());
+
+
+        //Concatenation
+        System.out.println("Enter first name");
+        String firstname;
+        firstname=sc.nextLine();
+        System.out.println("Enter Last name");
+        String lastname;
+        lastname=sc.nextLine();
+
+        System.out.println("Your name is => " +firstname +" "+lastname );
+
+
+
+        
     }
 }
